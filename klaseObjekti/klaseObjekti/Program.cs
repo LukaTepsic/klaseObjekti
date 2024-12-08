9591529186f831afcc92cbf8b39afbbfb81fc81f
LukaTepsic <166146790+LukaTepsic@users.noreply.github.com>
2024-12-08 23:17:59 +0100

    Add files via upload

---
+++ klaseObjekti/klaseObjekti/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Zrakoplov
+{
+    private string Naziv;
+    private double SnagaMotora;
+    private int DosegLeta;
+
+    public Zrakoplov(string naziv, double snagaMotora, int dosegLeta)
+    {
+        Naziv = naziv;
+        SnagaMotora = snagaMotora;
+        DosegLeta = dosegLeta;
+    }
+
+    public override string ToString()
+    {
+        return $"Naziv: {Naziv}, Snaga motora: {SnagaMotora} kW, Doseg leta: {DosegLeta} km";
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Zrakoplov x380 = new Zrakoplov("Airbus", 2000, 6000);
+        Zrakoplov C162 = new Zrakoplov("Cessna Skycatcher", 75, 870);
+        Zrakoplov PC21 = new Zrakoplov("Pilatus", 1200, 1333);
+
+        Console.WriteLine(x380.ToString());
+        Console.WriteLine(C162.ToString());
+        Console.WriteLine(PC21.ToString());
+    }
+}
